36e3634ff25d99487316b980e388c7a878116079
Gaurav Samar <gaurav.samar2000@gmail.com>
2025-07-21 19:54:08 +0530

    Adds CreateToDo component with input fields
    Implements the CreateToDo component to allow users
    to input a title and description for new tasks.
    This enhances the application's functionality by
    providing a user interface for adding to-do items.

---
+++ frontend/src/components/CreateToDo.jsx
@@ -0,0 +1,9 @@
+export function CreateToDo(){
+    return (
+        <div>
+            <input type="text" placeholder="Title"/>
+            <input type="text" placeholder="Description"/>
+            <button>Add A ToDo</button>
+        </div>
+    )
+}
